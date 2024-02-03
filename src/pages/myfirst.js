10aa9cf578ea5869506a1b8b36610f68de69862d
vludo <vladimir.osmekhin@gmail.com>
2024-02-03 15:10:10 +0200

    date-funktion optimointia ja kuvan ja nappulan siirtämistä

--- src/pages/myfirst.js
+++ src/pages/myfirst.js
@@ -1,47 +1,23 @@
 
 export function myDate () {
     let ajankohta = Date();
-    let vp = "", kk = "", pm = "", vuosi = "", aika = "", gmt = "";
+    let tiedot = ["","","","","",""]; // vp = "", kk = "", pm = "", vuosi = "", aika = "", gmt = "";
     let j = 0; 
     for (let i = 0; i < ajankohta.length; i++) {
         if (ajankohta[i] == " ") {
             j++;
             continue;
         }
-        switch (j) {
-            case 0:
-                vp += ajankohta[i];
-                break;
-            case 1:
-                kk += ajankohta[i];
-                break;
-            case 2:
-                pm += ajankohta[i];
-                break;
-            case 3:
-                vuosi += ajankohta[i];
-                break;
-            case 4:
-                aika += ajankohta[i];
-                break;
-            case 5:
-                gmt += ajankohta[i];
-                break;
-            break;
-        }
+        tiedot[j] += ajankohta[i];
     }
     
     let kaikkivp = {"Mon":"maanantai", "Tue":"tiistai", "Wed":"keskiviikko", "Thu":"torstai",
                     "Fri":"perjantai", "Sat":"lauantai", "Sun":"sunnuntai"};
     let kaikkikk = {"Jan":"01","Feb":"02","Mar":"03","Apr":"04","May":"05","Jun":"06","Jul":"07",
                     "Aug":"08","Sep":"09","Oct":"10","Nov":"11","Dec":"12"}
-    // let ulos = "viikonpäivä on: " + kaikkivp[vp] 
-    //      + "\npäivämäärä on: " + pm + "." + kaikkikk[kk] 
-    //      + "\nvuosi on: " + vuosi 
-    //      + "\naika on: " + aika
-    //      + "\ngmt: " + gmt;
-    
-    let ulos = {vp: kaikkivp[vp], pm, kk: kaikkikk[kk], vuosi, aika, gmt};
+
+    let ulos = {vp: kaikkivp[tiedot[0]], kk: kaikkikk[tiedot[1]], pm: tiedot[2],  
+                vuosi: tiedot[3], aika: tiedot[4], gmt: tiedot[5]};
     
     return ulos;
 }
